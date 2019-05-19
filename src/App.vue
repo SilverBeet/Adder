03fdcd5d3747ea1536ac3816a4b57804142dfe75
Silverbeet <haphka1@live.no>
2019-05-20 01:39:53 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<formApp/>
+	</div>
+</template>
+
+<script>
+	import formApp from './components/Form'
+
+	export default {
+		name: 'app',
+		components: {
+			formApp
+		}
+	}
+</script>
+
+<style>
+	* {
+		font-family: monospace;
+	}
+
+	body {
+		margin: 0;
+		padding: 0;
+		background: #813434;
+	}
+
+	#app {
+		display: block;
+	}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="tableWrapper">
+        <table>
+            <thead>
+                <tr>
+                    <th>Navn</th>
+                    <th>Timer</th>
+                </tr>
+            </thead>
+            <tbody class="tbody-data">
+                <tr :id="i" v-for="(person, i) in persons" :key="i">
+                    <td>{{person.name}}</td>
+                    <td>{{person.hours}}</td>
+                </tr>
+                <tr>
+                    <td colspan="2">Total timer:<span class="total">{{addRows(persons)}}</span> </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        name: 'table-app',
+
+        props: ['persons'],
+
+        methods: {
+            addRows(arr) {
+                let sum = 0;
+                arr.map((col, i) => sum += Number(arr[i].hours));
+                return sum;
+            }
+        }
+    }
+</script>
+
+<style scope="css">
+    table {
+        width: 550px;
+        margin: 50px auto;
+        color: rgba(165, 189, 30, 0.569);
+
+        border-collapse: collapse;
+        font-size: 15px;
+    }
+
+    .total {
+        float: right;
+    }
+    .totalText {
+        text-align: left;
+    }
+
+    th,
+    td {
+        padding: 10px;
+        text-align: left;
+        border: 1px solid rgba(165, 189, 30, 0.569);
+    }
+</style>
