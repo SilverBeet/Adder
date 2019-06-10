3e049f09db5171f9f645a6c5c82d5ca1a6425ebf
SilverBeet <46765471+SilverBeet@users.noreply.github.com>
2019-06-10 22:23:37 +0200

    Update Form.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,97 +1,108 @@
 <template>
-    <div>
-        <div class="formWrapper">
-            <form>
-                <div class="inputWrapper">
-                    <input required type="text" v-model="form.name" placeholder="Navn">
-                    <input required type="text" v-model="form.hours" placeholder="Timer">
-                    <input @click.prevent="sendData" type="submit" name="submit" class="submitBtn" value="Legg inn">
-                </div>
-            </form>
+  <div>
+    <div class="formWrapper">
+      <form>
+        <div class="inputWrapper">
+          <input required type="text" v-model="form.name" placeholder="Navn" />
+          <input
+            required
+            type="text"
+            v-model="form.hours"
+            placeholder="Timer"
+          />
+          <input
+            @click.prevent="sendData"
+            type="submit"
+            name="submit"
+            class="submitBtn"
+            value="Legg inn"
+          />
         </div>
-        <tableApp :persons="persons"></tableApp>
+      </form>
     </div>
+    <tableApp :persons="persons"></tableApp>
+  </div>
 </template>
 
 <script>
-    import tableApp from './Table'
-	export default {
-		name: 'form-app',
-        components: {
-            tableApp
-        },
-		data: () => {
-			return {
-                persons:[],
-				form: {
-					name: null,
-					hours: null
-				}
-			}
-		},
-        methods: {
-			sendData() {
-				this.persons.push({
-					'name': this.form.name,
-					'hours': this.form.hours,
-				})
-                this.form.name = this.form.hours = '';
-			}
-		}
-	}
+import tableApp from "./Table";
+export default {
+  name: "form-app",
+  components: {
+    tableApp
+  },
+  data: () => {
+    return {
+      persons: [],
+      form: {
+        name: null,
+        hours: null
+      }
+    };
+  },
+  methods: {
+    sendData() {
+      this.persons.push({
+        name: this.form.name,
+        hours: this.form.hours
+      });
+      this.form.name = this.form.hours = "";
+    }
+  }
+};
 </script>
 
 <style scope="css">
-	.formWrapper {
-		display: grid;
-		height: 100%;
-		width: 100%;
-		padding: 50px;
-		justify-content: center;
-		width: 450px;
-		height: auto;
-		background: transparent;
-		margin: 0 auto;
-		margin-top: 125px;
-		border: 1px solid rgba(165, 189, 30, 0.569);
-	}
+.formWrapper {
+  display: grid;
+  height: 100%;
+  width: 100%;
+  padding: 50px;
+  justify-content: center;
+  width: 450px;
+  height: auto;
+  background: transparent;
+  margin: 0 auto;
+  margin-top: 125px;
+  border: 1px solid rgba(165, 189, 30, 0.569);
+}
 
-	.inputWrapper {
-		display: grid;
-		grid-gap: 10px;
-	}
+.inputWrapper {
+  display: grid;
+  grid-gap: 10px;
+}
 
-	input[type="text"] {
-		font-family: monospace;
-		display: block;
-		background-color: transparent;
-		border: none;
-		border-bottom: 1px solid rgba(165, 189, 30, 0.569);
-		margin: 0 auto;
-		width: 250px;
-		padding: 5px;
-		padding-left: 0;
-		font-size: 15px;
-		outline:none;
-		color: rgba(165, 189, 30, 0.569);
-	}
+input[type="text"] {
+  font-family: monospace;
+  display: block;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid rgba(165, 189, 30, 0.569);
+  margin: 0 auto;
+  width: 250px;
+  padding: 5px;
+  padding-left: 0;
+  font-size: 15px;
+  outline: none;
+  color: rgba(165, 189, 30, 0.569);
+}
 
-	input::placeholder {
-		opacity: .6;
-		color: rgba(165, 189, 30, 0.569);
-	}
+input::placeholder {
+  opacity: 0.6;
+  color: rgba(165, 189, 30, 0.569);
+}
 
-	.submitBtn {
-		display: block;
-		font-family: monospace;
-		font-size: 15px;
-		margin: 0 auto;
-		padding: 5px;
-		width: 150px;
-		border: 1px solid rgba(165, 189, 30, 0.569);
-		margin-top: 40px;
-		color: rgba(165, 189, 30, 0.569);
-		outline: none;
-		background-color: transparent;
-	}
+.submitBtn {
+  display: block;
+  font-family: monospace;
+  font-size: 15px;
+  margin: 0 auto;
+  padding: 5px;
+  width: 150px;
+  border: 1px solid rgba(165, 189, 30, 0.569);
+  margin-top: 40px;
+  color: rgba(165, 189, 30, 0.569);
+  outline: none;
+  background-color: transparent;
+}
 </style>
