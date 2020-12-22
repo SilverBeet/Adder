dafaf3f76e21b20d63ccff22cf7d027ed632d7f8
Silverbeet <46765471+SilverBeet@users.noreply.github.com>
2020-12-22 13:50:35 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-	<div id="app">
-		<formApp/>
-	</div>
+  <div id="app">
+    <formApp/>
+  </div>
 </template>
 
 <script>
-	import formApp from './components/Form'
+  import formApp from './components/Form'
 
-	export default {
-		name: 'app',
-		components: {
-			formApp
-		}
-	}
+  export default {
+    name: 'app',
+    components: {
+      formApp
+    }
+  }
 </script>
 
 <style>
-	* {
-		font-family: monospace;
-	}
+* {
+  font-family: monospace;
+}
 
-	body {
-		margin: 0;
-		padding: 0;
-		background: #813434;
-	}
+body {
+  margin: 0;
+  padding: 0;
+  background: #813434;
+}
 
-	#app {
-		display: block;
-	}
+#app {
+  display: block;
+}
 </style>
